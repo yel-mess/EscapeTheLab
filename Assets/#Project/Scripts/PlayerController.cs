6e286d711f668b30f3c1ac5ba26182c00c8d8982
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-09-27 16:48:45 +0200

    tried to add speed

--- Assets/#Project/Scripts/PlayerController.cs
+++ Assets/#Project/Scripts/PlayerController.cs
@@ -7,28 +7,46 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private bool leftClickPressed = false;
-    public float movementSpeed;
-    private bool isMoving;
+    public float movementSpeed = 0f;
+    //private bool isMoving;
     Vector2 lastClickedPos;
     private SpriteRenderer spriteRenderer;
+
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
         //animator = GetComponent<Animator>();
     }
+
+    // Move to the target end position.
     void Update() {
         if(leftClickPressed) {
             //SetTargetPosition(); // méthode pour enregistrer la position de la souris
             
             //récupère la position de la souris et bouge le joueur
             lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            isMoving = true;
-            if(isMoving && (Vector2)transform.position != lastClickedPos) {
-                float step = movementSpeed * Time.deltaTime;
-                transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+
+            if (lastClickedPos.x > transform.position.x) {
+                //bouger à droite
+                Debug.Log("Je vais à droite");
+                float deplacement = movementSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, deplacement);
+                //transform.position = new Vector2(lastClickedPos.x, transform.position.y);
             }
             else {
-                isMoving = false;
+                //bouger à gauche
+                Debug.Log("Je vais à gauche");
+                transform.position = new Vector2(lastClickedPos.x, transform.position.y);
             }
+
+            //isMoving = true;
+            //if(isMoving && (Vector2)transform.position != lastClickedPos) {
+            //    float step = movementSpeed * Time.deltaTime;
+            //    transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+            //}
+            //else {
+            //    isMoving = false;
+            //}
+
             //Vector2 movement = new Vector2(mousePos.x, transform.position.y) * movementSpeed;
 
             //transform.position = new Vector2(mousePos.x, transform.position.y);
