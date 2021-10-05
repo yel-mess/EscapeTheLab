dd0375946c1c7e89869f64313da5ee406c0b1d0d
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-10-05 16:49:33 +0200

    tried to add speed again twice

--- Assets/#Project/Scripts/PlayerController.cs
+++ Assets/#Project/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     private bool leftClickPressed = false;
     public float movementSpeed = 0f;
-    //private bool isMoving;
+    //private bool isMoving = false;
     Vector2 lastClickedPos;
     //Vector2 destination;
     private SpriteRenderer spriteRenderer;
@@ -21,17 +21,26 @@ public class PlayerController : MonoBehaviour
 
     // Move to the target end position.
     void Update() {
+
+        float deplacement = movementSpeed * Time.deltaTime;
+
         if(leftClickPressed) {
             
             //récupère la position de la souris et bouge le joueur
             lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            float deplacement = movementSpeed * Time.deltaTime;
 
             if (lastClickedPos.x > transform.position.x) {
 
                 //bouger à droite
-                Debug.Log("Je vais à droite");
-                transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, deplacement);
+
+                // Debug.Log("Je vais à droite");
+                // Vector2 right = Vector2.right;
+                // Vector2 translation = right * deplacement;
+                // transform.Translate(translation);
+
+                transform.position = Vector2.Lerp(transform.position, lastClickedPos, deplacement);
+
+                //transform.position = Vector2.Lerp(transform.position, lastClickedPos, deplacement);
                 //transform.position = new Vector2(lastClickedPos.x, transform.position.y);
             }
             else {
@@ -39,9 +48,8 @@ public class PlayerController : MonoBehaviour
                 //bouger à gauche
                 Debug.Log("Je vais à gauche");
                 //transform.position = new Vector2(lastClickedPos.x, transform.position.y);
-                transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, deplacement);
+                transform.position = Vector2.Lerp(transform.position, lastClickedPos, deplacement);
             }
-
             //Vector2 movement = new Vector2(mousePos.x, transform.position.y) * movementSpeed;
 
             //transform.position = new Vector2(mousePos.x, transform.position.y);
@@ -51,11 +59,11 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context) {
         if (context.performed) {
             leftClickPressed = true;
-            Debug.Log("OK !");
         }
     }
     public void Interact(InputAction.CallbackContext context) {
         if (context.performed)
             Debug.Log("I'm doing something !");
     }
+    
 }
