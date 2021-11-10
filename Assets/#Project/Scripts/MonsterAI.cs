144409aa71af9ddd38ca27aaaeeee6a31915a8e0
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-11-10 12:27:17 +0100

    added enemy and animation

--- Assets/#Project/Scripts/MonsterAI.cs
+++ Assets/#Project/Scripts/MonsterAI.cs
@@ -10,31 +10,71 @@ public class MonsterAI : MonoBehaviour
     [SerializeField]
     float deathRange;
 
-    [SerializeField]
-    float moveSpeed;
+    public Vector2 moveSpeed = Vector2.zero;
     Rigidbody2D rb2d; //il faut pouvoir accéder au rigidBody
+    SpriteRenderer spriteRenderer;
+    public float hitRange;
+    Animator animator;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        Vector2 start;
+        Vector2 direction;
+
         //distance to player
         float distToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if(moveSpeed.x < 0) {
+            if(animator != null) {
+                //flip
+                animator.SetBool("Right", false);
+            }
+            else {
+                spriteRenderer.flipX = false;
+            }
+            start = (Vector2)transform.position + Vector2.left * 0.51f;
+            direction = Vector2.left;
+        }
+        else {
+            if (animator != null) { //si pas d'animation, flippe
+                animator.SetBool("Right", true);
+            }
+            else {
+                spriteRenderer.flipX = true;
+            }
+            //change if problems occur
+            spriteRenderer.flipX = false;
+            start = (Vector2)transform.position + Vector2.right * 0.51f;
+            direction = Vector2.right;
+        }
 
         //monster needs to follow the player after it emerges from hiding spots
         //use ray cast ? (check 2DGameThingy)
 
+        Debug.DrawRay(start, direction * hitRange, Color.blue);
+        RaycastHit2D hit = Physics2D.Raycast(start, direction, hitRange);
+
+        Vector2 deplacement = moveSpeed * Time.deltaTime;
+        transform.position += (Vector3)deplacement;
+
         if(distToPlayer < deathRange){
-            //kills the player the player
+            //kills the player
         }
     }
     void ChasePlayer(){
 
     }
     void StopChasingPlayer(){
+
+    }
+    void KillPlayer(){
         
     }
 }
