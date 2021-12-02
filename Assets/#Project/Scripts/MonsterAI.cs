769bee3d05ee4d5c3472b7100ba95800dbd21dda
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-12-02 07:01:46 +0100

    enemy patrols from left to right

--- Assets/#Project/Scripts/MonsterAI.cs
+++ Assets/#Project/Scripts/MonsterAI.cs
@@ -3,49 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class MonsterAI : MonoBehaviour
 {
-    float dirX;
-    float moveSpeed;
-    Rigidbody2D rb2d; //il faut pouvoir accéder au rigidBody
-    bool facingRight = false;
-    Vector2 localScale;
-    Animator animator;
-    public string sceneName;
-    GameObject player; //keep track of the player
-    SpriteRenderer spriteRenderer;
-    //public GameObject ignoreObjects;
+    //Reference to waypoints
+    public List<Transform> points;
+    //The int value for next point index
+    public int nextID = 0;
+    //The value of that applies to ID for changing
+    int idChangeValue = 1;
+    //Speed of movement
+    public float speed = 2;
 
-    void Start()
+    void Reset()
     {
-        localScale = transform.localScale;
-        rb2d = GetComponent<Rigidbody2D>();
-        dirX = -1f;
-        moveSpeed = 3f;
-        player = GameObject.FindGameObjectWithTag("Player");
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-        //Physics2D.IgnoreCollision(GetComponent<Collider2D>(), ignoreObjects.GetComponent<Collider2D>());
+        Init();
     }
-    void OnTriggerEnter2D(Collider2D collision){
-        if(collision.CompareTag("Wall")){
-            dirX *= -1f;
+
+    void Init()
+    {
+        //Make box collider trigger
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        //Create Root object
+        GameObject root = new GameObject(name + "_Root");
+        //Reset Position of Root to enemy object
+        root.transform.position = transform.position;
+        //Set enemy object as child of root
+        transform.SetParent(root.transform);
+        //Create Waypoints object
+        GameObject waypoints = new GameObject("Waypoints");
+        //reset waypoints position to root
+        //Make waypoints object child of root
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+        //Create two points (gameobject) and reset their position to waypoints objects
+        //make the points children of waypoint object
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = root.transform.position;
+
+        //Init points list then add the points to it
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
+
+    void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+        //Get the next Point transform
+        Transform goalPoint = points[nextID];
+        //Flip the enemy trensform to look into the point's direction
+        if(goalPoint.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
-    }
-    void FixedUpdate()
-    {
-        rb2d.velocity = new Vector2(dirX *moveSpeed, rb2d.velocity.y);
-    }
-    void LateUpdate(){
-        CheckWhereToFace();
-    }
-    void CheckWhereToFace(){
-        if(dirX > 0)
-            facingRight = true;
-        else if (dirX < 0)
-            facingRight = false;
-        if(((facingRight) && (localScale.x < 0)) || ((facingRight) && (localScale.x > 0)))
-            localScale.x *= -1;
-        transform.localScale = localScale;
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        //Move the enemy towards the goal point
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed*Time.deltaTime);
+        //Check the distance between enemy and goal point to trigger next point
+        if(Vector2.Distance(transform.position, goalPoint.position) < 1f)
+        {
+            //Check if we are at the end of the line (make the change -1)
+            if(nextID == points.Count - 1)
+            {
+                idChangeValue = -1;
+            }
+            //Check if we are at the start of the line (make the change +1)
+            if(nextID == 0)
+            {
+                idChangeValue = 1;
+            }
+            //Apply the change on the nextID
+            nextID += idChangeValue;
+        }
     }
 }
