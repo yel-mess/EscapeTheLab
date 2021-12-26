9e8d12a5630ec96aebf8f122f8bf65f59735a97f
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-12-26 12:13:01 +0100

    changed typos, deleted some scripts and changed audio script

--- Assets/#Project/Scripts/Audio.cs
+++ Assets/#Project/Scripts/Audio.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class Audio : MonoBehaviour
 {
+    public static Audio instance;
     void Awake()
     {
-
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy (gameObject);
+            return;
+        }
         DontDestroyOnLoad(gameObject);
     }
 }
